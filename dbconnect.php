6eeae59778cd69ed62a9830e6019190b3fa718ac
krishnabattersea <142780413+krishnabattersea@users.noreply.github.com>
2023-08-21 13:12:56 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -4,7 +4,7 @@ function OpenCon()
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$db = "push_noti";
+$dbname = "push_noti";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname) or die("Connect failed: %s\n". $conn -> error);
 return $conn;
 }
