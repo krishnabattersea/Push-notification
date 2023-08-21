7ce941bd8f651451604db3cd53310264dd966048
krishnabattersea <142780413+krishnabattersea@users.noreply.github.com>
2023-08-21 13:09:58 +0530

    Update push-notify.php

--- push-notify.php
+++ push-notify.php
@@ -1,5 +1,8 @@
 <?php
 
+include './dbconnect.php';
+$conn = OpenCon();
+
 // if there is anything to notify, then return the response with data for
 // push notification else just exit the code
 $webNotificationPayload['title'] = 'Push Notification from GetAssist';
@@ -10,6 +13,34 @@ echo json_encode($webNotificationPayload);
 exit();
 
 
+
+$query = "SELECT * FROM `details`;";
+  
+  // FETCHING DATA FROM DATABASE
+  $result = $conn->query($query);
+  echo result;
+    // if ($result->num_rows > 0) 
+    // {
+    //     // OUTPUT DATA OF EACH ROW
+    //     while($row = $result->fetch_assoc())
+    //     {
+    //         echo "id: " .
+    //             $row["id"]. " - body: " .
+    //             $row["body"]. " | title: " . 
+    //             $row["title"]. " | payload: " . 
+    //             $row["payload"]. "<br>";
+    //     }
+    // } 
+    // else {
+    //     echo "0 results";
+    // }
+  
+ 
+
+
+
+
+CloseCon($conn);
 
 
 
