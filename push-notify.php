227b8e4869c266f4239f6f9d37577a11bb168b15
krishnabattersea <142780413+krishnabattersea@users.noreply.github.com>
2023-08-21 16:35:46 +0530

    Update push-notify.php

--- push-notify.php
+++ push-notify.php
@@ -1,47 +1,18 @@
+
+
 <?php
 
-include './dbconnect.php';
-$conn = OpenCon();
+
+include './fetchdata.php';
 
 // if there is anything to notify, then return the response with data for
 // push notification else just exit the code
-$webNotificationPayload['title'] = 'Push Notification from GetAssist';
-$webNotificationPayload['body'] = 'Open browser to view.';
-$webNotificationPayload['icon'] = './GAne.png';
-$webNotificationPayload['url'] = 'https://getassist.net/';
+$webNotificationPayload['title'] = $title;
+$webNotificationPayload['body'] = $body;
+$webNotificationPayload['icon'] = $icon;
+$webNotificationPayload['url'] = $payload;
 echo json_encode($webNotificationPayload);
 exit();
-
-
-
-$query = "SELECT * FROM `details`;";
-  
-  // FETCHING DATA FROM DATABASE
-  $result = $conn->query($query);
-  echo result;
-    // if ($result->num_rows > 0) 
-    // {
-    //     // OUTPUT DATA OF EACH ROW
-    //     while($row = $result->fetch_assoc())
-    //     {
-    //         echo "id: " .
-    //             $row["id"]. " - body: " .
-    //             $row["body"]. " | title: " . 
-    //             $row["title"]. " | payload: " . 
-    //             $row["payload"]. "<br>";
-    //     }
-    // } 
-    // else {
-    //     echo "0 results";
-    // }
-  
- 
-
-
-
-
-CloseCon($conn);
-
 
 
 
